14307543e7b880b70887fba977eac51f907a4192
Marcos Vinicius <marcos_irigojen@outlook.com>
2021-12-06 16:13:29 -0400

    criação da querie de pedido

---
+++ src/NerdStore.WebApp.MVC/Extensions/CartViewComponent.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using NerdStore.Vendas.Application.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerdStore.WebApp.MVC.Extensions
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly IPedidoQueries _pedidoQueries;
+
+        // TODO: Obter cliente logado
+        protected Guid ClienteId = Guid.Parse("54f589fc-2f22-4980-a097-e29828020119");
+
+
+        public CartViewComponent(IPedidoQueries pedidoQueries)
+        {
+            _pedidoQueries = pedidoQueries;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var carrinho = await _pedidoQueries.ObterCarrinhoCliente(ClienteId);
+            var itens = carrinho?.Items.Count ?? 0;
+
+            return View(itens);
+        }
+    }
+}
